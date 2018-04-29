006abe5ba8d61f32884da7b5290fcb0aaf66607e
akecka <agnkecka@gmail.com>
2018-04-29 18:53:46 +0200

    android

---
+++ app/src/main/java/com/borsuk/connect3/MainActivity.java
@@ -0,0 +1,39 @@
+package com.borsuk.connect3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //0 = yellow, 1 = red;
+
+    int activePlayer = 0;
+
+    public void dropin(View view) {
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+
+        if (activePlayer == 0) {
+
+            counter.setImageResource(R.drawable.yello);
+            activePlayer = 1;
+
+        } else {
+
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
+
+        counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+    }
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+    }
+
